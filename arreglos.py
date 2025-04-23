b91cebd664def72c8e6f1716cf4cfd4ee944e168
Luiz <kitt.man.2002@gmail.com>
2025-04-23 11:16:47 -0400

    otrocod

---
+++ arreglos.py
@@ -0,0 +1,3 @@
+nombre = "carlos" #String
+edad = 30 #Entero int
+estatura =
---
+++ arreglos2.py
@@ -0,0 +1,21 @@
+playlistdelcurso = ["Beat-it.mp3" , "Lova.mp3" , "15b.mp3" , "Diva virtual.mp3" , "Pokemon Johtho.mp3"]
+print(playlistdelcurso)
+
+print("#####")
+print(playlistdelcurso[3])
+
+print ("TAMAÑO DEL ARREGLO")
+listSize = len(playlistdelcurso)
+print(listSize)
+print("####---La Ultima cancion es-----#####")
+print(playlistdelcurso[listSize - 1])
+
+print("otro ejmeplo")
+palabra = "Maxi"
+print(len(palabra))
+
+print("---- imprimir todas las letras de una palabra ----")
+nuevapalabra = "Girafarig"
+
+for i in range(len(nuevapalabra)):
+    print(nuevapalabra[i])
